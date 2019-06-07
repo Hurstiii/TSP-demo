dd02e1d1af0ff4df44f9679dee8a8c71542e4517
George <psygh3@nottingham.ac.uk>
2019-06-07 17:19:22 +0100

    initialise the project repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+import Sigma, { RelativeSize, SigmaEnableWebGL } from "react-sigma";
+import square from "./square.json";
+import "./App.css";
+import MySigma from "./MySigma";
+import RandomizeNodePositions from "react-sigma/lib/RandomizeNodePositions";
 
-function App() {
+const App = () => {
+  const initialGraph = square;
+  initialGraph.timestamp = moment().unix();
+
+  const [graph, setGraph] = useState(square);
+  const handleGraphChange = () => {
+    try {
+      let data = JSON.parse(text);
+      data.timestamp = moment().unix();
+      setGraph(data);
+    } catch (e) {
+      //TODO: warn user of invalid input
+      console.log("invalid JSON");
+      console.log(e);
+    }
+  };
+
+  const [text, setText] = useState();
+  const handleTextChange = e => {
+    setText(e.target.value);
+  };
+
+  const debug = () => {
+    console.log(graph);
+    console.log(text);
+    console.log();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div id="header">
+        <div id="title" className="h1">
+          Sigma test
+        </div>
+      </div>
+      {/* 
+        //TODO: Prettify the look 
+      */}
+      <div id="UI">
+        <textarea
+          cols="50"
+          rows="30"
+          value={text}
+          onChange={handleTextChange}
+        />
+        <button value="Upload" onClick={handleGraphChange}>
+          Upload
+        </button>
+        <button onClick={debug}>Debug</button>
+      </div>
+      <div id="graph">
+        <Sigma
+          style={{ background: "#fcfcfc", height: "100%" }}
+          settings={{
+            drawEdges: true,
+            clone: true,
+            immutable: false,
+            /* defaultEdgeColor: "#4d4e4f",
+            defaultNodeColor: "#4d4e4f", */
+            edgeColor: "default",
+            touchEnabled: false,
+            mouseEnabled: false,
+            sideMargin: 1
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <MySigma graph={graph}>
+            <RelativeSize initialSize={15} key={graph.timestamp} />
+          </MySigma>
+        </Sigma>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
